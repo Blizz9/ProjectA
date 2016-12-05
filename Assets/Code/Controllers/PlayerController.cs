eea04816012097e19da63e7fb43c6f103f5c32f9
Nick <blizzard@gmail.com>
2016-12-05 02:44:04 -0500

    Added a little script example of Mario moving around

---
+++ Assets/Code/Controllers/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("ProjectA/Controllers/Player Controller")]
+public class PlayerController : MonoBehaviour
+{
+    #region MonoBehaviour
+
+    public void Awake()
+    {
+    }
+
+    public void Start()
+    {
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 6f), ForceMode2D.Impulse);
+
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-5f, 0f), ForceMode2D.Force);
+        }
+        else if (Input.GetAxis("Horizontal") > 0f)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(5f, 0f), ForceMode2D.Force);
+        }
+    }
+
+    public void LateUpdate()
+    {
+    }
+
+    #endregion
+}
