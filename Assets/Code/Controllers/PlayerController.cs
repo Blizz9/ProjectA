f10061025834e9488f26ff3adb43e6e39fc36ce5
Nick <blizzard@gmail.com>
2016-12-10 12:14:14 -0500

    Added initial work on item interaction

--- Assets/Code/Controllers/PlayerController.cs
+++ Assets/Code/Controllers/PlayerController.cs
@@ -1,9 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 [AddComponentMenu("Ludum Dare 37/Controllers/Player Controller")]
 public class PlayerController : MonoBehaviour
 {
     private const float MOVE_RATE = 3f;
+
+    private bool _ignoreNextTriggerEnter;
 
     #region MonoBehaviour
 
@@ -29,6 +32,50 @@ public class PlayerController : MonoBehaviour
         {
             float y = transform.localPosition.y + MOVE_RATE * Time.deltaTime;
             transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+        }
+
+        // stub out what it is like to drop an item, we will use this later
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject key = gameObject.SearchHierarchy(HierarchySearchType.Children, true, "Key").FirstOrDefault();
+
+            if (key != null)
+            {
+                GameObject room1 = gameObject.SearchHierarchy(HierarchySearchType.Siblings, true, "Room1").First();
+                GameObject room1DoorTile = room1.SearchHierarchy(HierarchySearchType.Descendants, true, "DoorTile").First();
+                room1DoorTile.GetComponent<Collider2D>().isTrigger = false;
+
+                key.transform.parent = room1.transform;
+                key.transform.localScale = Vector3.one;
+
+                _ignoreNextTriggerEnter = true;
+                key.GetComponent<Collider2D>().enabled = true;
+            }
+        }
+    }
+
+    #endregion
+
+    #region Collisions
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_ignoreNextTriggerEnter)
+        {
+            _ignoreNextTriggerEnter = false;
+        }
+        else
+        {
+            GameObject key = collision.gameObject;
+
+            key.transform.parent = gameObject.transform;
+            key.transform.localPosition = Vector3.zero;
+            key.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
+            key.GetComponent<Collider2D>().enabled = false;
+
+            GameObject room1 = gameObject.SearchHierarchy(HierarchySearchType.Siblings, true, "Room1").First();
+            GameObject room1DoorTile = room1.SearchHierarchy(HierarchySearchType.Descendants, true, "DoorTile").First();
+            room1DoorTile.GetComponent<Collider2D>().isTrigger = true;
         }
     }
 
